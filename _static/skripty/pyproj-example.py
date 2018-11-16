5c48718bc5220e04463dae41171e4f048656c1c8
Martin Landa <landa.martin@gmail.com>
2018-11-16 14:47:21 +0100

    pyproj example fix

--- _static/skripty/pyproj-example.py
+++ _static/skripty/pyproj-example.py
@@ -1,4 +1,6 @@
 import pyproj
+
 sjtsk = pyproj.Proj("+init=epsg:5514")
 wgs = pyproj.Proj("+init=epsg:4326")
-print (sjtsk(-868208.53, -1095793.57, inverse=True))
+
+print (pyproj.transform(sjtsk, wgs, -868208.53, -1095793.57))
