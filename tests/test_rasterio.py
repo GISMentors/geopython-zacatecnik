ef7eadb85ebdda307c8472f50ad211fa83d679b8
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-06-05 14:33:58 +0200

    Fixed data sources. Fixed tests.

--- tests/test_rasterio.py
+++ tests/test_rasterio.py
@@ -1,23 +1,14 @@
-import urllib.request
 import rasterio
 from rasterio.windows import Window
 import rasterio.features
 import json, os
 
 class TestRaster:
-    def pytest_sessionstart(self):
-        self.get_file('B04-2018-05-06.tiff')
-        self.get_file('B08-2018-05-06.tiff')
-
-    def get_file(self, filename):
-        url = 'https://github.com/GISMentors/geopython-zacatecnik/raw/master/data/' + filename
-        urllib.request.urlretrieve(url, filename)
-
     def create_ndvi(self):
-        with rasterio.open('B04-2018-05-06.tiff') as vis:
+        with rasterio.open('data/B04-2018-05-06.tiff') as vis:
             vis_data = vis.read().astype(float)[0]
 
-        with rasterio.open('B08-2018-05-06.tiff') as nir:
+        with rasterio.open('data/B08-2018-05-06.tiff') as nir:
             nir_data = nir.read().astype(float)[0]
 
         ndvi = (nir_data - vis_data) / (nir_data + vis_data)
@@ -34,12 +25,12 @@ class TestRaster:
             "compress": "lzw"
         }
 
-        with rasterio.open('ndvi.tif', 'w', **kwargs) as dst:
+        with rasterio.open('data/ndvi.tif', 'w', **kwargs) as dst:
             dst.write_band(1, ndvi.astype(rasterio.float32))
 
     def create_ndvi_classes(self):
         self.create_ndvi()
-        with rasterio.open("ndvi.tif") as src:
+        with rasterio.open("data/ndvi.tif") as src:
             data = src.read(1)
 
             # reklasifikace začíná
@@ -51,35 +42,35 @@ class TestRaster:
             # zápis do souboru
             meta = src.meta
             meta.update(dtype=rasterio.int16, count=1, compress='lzw')
-            with rasterio.open('ndvi-classes.tif', 'w', **meta) as dst:
+            with rasterio.open('data/ndvi-classes.tif', 'w', **meta) as dst:
                 dst.write_band(1, data.astype(rasterio.int16))
 
     def test_rasterio_read(self):
-        red = rasterio.open('B04-2018-05-06.tiff')
-        assert str(red.bounds) == 'BoundingBox(left=14.513969421386719, bottom=48.66330341686951, right=15.073928833007814, top=48.866521538507754)'
+        red = rasterio.open('data/B04-2018-05-06.tiff')
+        assert red.bounds[0] > 14.69469421 and red.bounds[0] < 14.69469422
 
-        with rasterio.open('B04-2018-05-06.tiff') as vis:
+        with rasterio.open('data/B04-2018-05-06.tiff') as vis:
             vis_data = vis.read().astype(float)[0]
 
-        with rasterio.open('B08-2018-05-06.tiff') as nir:
+        with rasterio.open('data/B08-2018-05-06.tiff') as nir:
             nir_data = nir.read().astype(float)[0]
 
         ndvi = (nir_data - vis_data) / (nir_data + vis_data)
 
-        assert ndvi.min() == -0.4162768343479654
+        assert ndvi.min() < -0.05759017 and ndvi.min() > -0.05759018
 
     def test_rasterio_write(self):
         self.create_ndvi()
 
     def test_rasterio_window_read(self):
-        with rasterio.open('B04-2018-05-06.tiff') as red:
-            w = red.read(1, window=Window(0, 0, 512, 256))
+        with rasterio.open('data/B04-2018-05-06.tiff') as red:
+            w = red.read(1, window=Window(0, 0, 256, 128))
 
-        assert w.shape[0] == 256
+        assert w.shape[0] == 128
 
     def test_rasterio_vectorize(self):
         self.create_ndvi_classes()
-        with rasterio.open("ndvi-classes.tif") as src:
+        with rasterio.open("data/ndvi-classes.tif") as src:
             data = src.read(1)
 
             # čištění dat
@@ -115,7 +106,9 @@ class TestRaster:
                     features["features"].append(feature)
 
             # zápis do souboru
-            with open("ndvi-classes.geojson", "w") as out:
+            with open("data/ndvi-classes.geojson", "w") as out:
                 json.dump(features, out)
 
-            assert os.stat('ndvi-classes.geojson').st_size == 25189696
+            with open("data/ndvi-classes.geojson", "r") as infile:
+                data = json.load(infile)
+                assert len(data['features']) == 23
