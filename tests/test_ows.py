5cfe1db3081fa302fdcada80a50519d80a4fad57
Jachym Cepicky <jachym.cepicky@gmail.com>
2020-06-05 13:29:22 +0200

    initializing tests

---
+++ tests/test_ows.py
@@ -0,0 +1,29 @@
+from owslib.wms import WebMapService as WMS
+from owslib.csw import CatalogueServiceWeb as CSW
+
+def test_wms():
+    url = "https://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx"
+    wms = WMS(url)
+
+    assert wms
+
+    response = wms.getmap(
+            size=[800, 800],
+            srs="epsg:4326",
+            bbox=[15, 50, 15.5, 50.5],
+            format="image/jpeg",
+            layers=["GR_ORTFOTORGB"]
+            )
+
+    assert response
+    assert int(response.info()["Content-Length"])
+
+def test_csw():
+    url = "http://micka.cenia.cz/metadata/csw/index.php"
+    csw = CSW(url)
+
+    assert csw
+
+    csw.getrecords2()
+
+    assert csw.results["matches"] > 0
